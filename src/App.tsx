fb47f0d487fdf0c650031cce8e3279eb443730db
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-03-28 10:39:57 -0300

    feat(app): added new About and Home Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import About from './components/About/About'
+import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar show={false}/>
-      <h1>texte</h1>
+      <Home />
+      <About />
     </div>
   )
 }
