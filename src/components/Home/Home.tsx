631ef09667c2e291251e093a24dd21098e694db9
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-03-30 10:49:39 -0300

    chore(home): changes in colors because the palette was changed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,8 +8,8 @@ const Home = () => {
         <div className='home'>
             <div className="brief-about">
                 <p className='helo'>Olá! Meu nome é,</p>
-                <h1 className='myname'>Lucas Gomes </h1>
-                <div className="brief-descrition">
+                <h1 className='myname'>Lucas <b>Gomes</b></h1>
+                <div className="brief-description">
                     <p>Sou um desenvolvedor Front-end em início de carreira. <br />
                 Tenho habilidades em Git, GitHub, React, TypeScript, JavaScript, HTML e CSS, adquiridas por meio de estudos e projetos pessoais. <br />
                 Sou uma pessoa curiosa, comprometida e disposta a aprender constantemente para aprimorar minhas habilidades e contribuir com soluções eficientes. <br />
