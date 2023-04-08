ff270e11c673ffce0dcacfd229dde758928edac6
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-04-08 18:04:32 -0300

    feat(home): styling added contact icons to facilitate contact

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import ProfilePhoto1 from '../../assets/img/foto1.png'
 import ProfilePhoto2 from '../../assets/img/foto2.png'
+import { BsLinkedin, BsGithub, BsWhatsapp} from 'react-icons/bs'
 
 const Home = () => {
     return (
@@ -11,9 +12,14 @@ const Home = () => {
                 <h1 className='myname'>Lucas <b>Gomes</b></h1>
                 <div className="brief-description">
                     <p>Sou um desenvolvedor Front-end em início de carreira. <br />
-                Tenho habilidades em Git, GitHub, React, TypeScript, JavaScript, HTML e CSS, adquiridas por meio de estudos e projetos pessoais. <br />
-                Sou uma pessoa curiosa, comprometida e disposta a aprender constantemente para aprimorar minhas habilidades e contribuir com soluções eficientes. <br />
-                Gosto de trabalhar em equipe e estou em busca de oportunidades para crescer profissionalmente e aplicar minhas habilidades de forma criativa.</p>
+                        Tenho habilidades em Git, GitHub, React, TypeScript, JavaScript, HTML e CSS, adquiridas por meio de estudos e projetos pessoais. <br />
+                        Sou uma pessoa curiosa, comprometida e disposta a aprender constantemente para aprimorar minhas habilidades e contribuir com soluções eficientes. <br />
+                        Gosto de trabalhar em equipe e estou em busca de oportunidades para crescer profissionalmente e aplicar minhas habilidades de forma criativa.</p>
+                </div>
+                <div className="icons">
+                    <BsLinkedin className='linkedin'/>
+                    <BsGithub className='github'/>
+                    <BsWhatsapp className='whatsapp'/>
                 </div>
             </div>
             <div className="profile-photo">
